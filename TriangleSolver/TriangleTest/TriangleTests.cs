30dc637c7f48b1c19a677fe67632b976186e89fd
LoganManery <logan.manery@outlook.com>
2023-02-10 14:51:58 -0500

    Scalene Tests

--- TriangleSolver/TriangleTest/TriangleTests.cs
+++ TriangleSolver/TriangleTest/TriangleTests.cs
@@ -15,23 +15,79 @@ namespace TriangleTest
         [TestFixture]
         public class EquilateralTest
         {
-
+            
         }
 
         [TestFixture]
         public class IsoscelesTest
         {
-
+            
         }
 
         [TestFixture]
         public class ScaleneTest
         {
             [Test]
-            public void TriangleWithThreeUnequalSides_IsScalene()
+            public void AnalyzeTriangle_ValidScalene_3_4_5()
             {
                 // Arrange
-                var triangle = new Triangle(4, 5, 6);
+                var triangle = new Triangle(3, 4, 5);
+
+                // Act
+                string scaleneRegex = @"SCALENE$";
+                Regex re = new Regex(scaleneRegex);
+                bool isScalene = re.IsMatch(triangle.AnalyzeTriangle());
+
+                // Assert
+                Assert.IsTrue(isScalene);
+            }
+
+            [Test]
+            public void AnalyzeTriangle_ValidScalene_5_12_13() 
+            {
+                var triangle = new Triangle(5, 12, 13);
+
+                // Act
+                string scaleneRegex = @"SCALENE$";
+                Regex re = new Regex(scaleneRegex);
+                bool isScalene = re.IsMatch(triangle.AnalyzeTriangle());
+
+                // Assert
+                Assert.IsTrue(isScalene);
+            }
+
+            [Test]
+            public void AnalyzeTriangle_ValidScalene_8_15_17()
+            {
+                var triangle = new Triangle(8, 15, 17);
+
+                // Act
+                string scaleneRegex = @"SCALENE$";
+                Regex re = new Regex(scaleneRegex);
+                bool isScalene = re.IsMatch(triangle.AnalyzeTriangle());
+
+                // Assert
+                Assert.IsTrue(isScalene);
+            }
+
+            [Test]
+            public void AnalyzeTriangle_ValidScalene_7_24_25()
+            {
+                var triangle = new Triangle(7, 24, 25);
+
+                // Act
+                string scaleneRegex = @"SCALENE$";
+                Regex re = new Regex(scaleneRegex);
+                bool isScalene = re.IsMatch(triangle.AnalyzeTriangle());
+
+                // Assert
+                Assert.IsTrue(isScalene);
+            }
+
+            [Test]
+            public void AnalyzeTriangle_ValidScalene_10_24_26()
+            {
+                var triangle = new Triangle(10, 24, 26);
 
                 // Act
                 string scaleneRegex = @"SCALENE$";
